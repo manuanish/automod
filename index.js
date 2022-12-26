60fba1fbf0d47f038836651ed646d15da55e36aa
Manu Anish <manusanish@gmail.com>
2022-12-26 20:07:32 +0530

    stuff

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('ok');
+require("dotenv").config();
+
+const {
+  Client,
+  GatewayIntentBits,
+  Events,
+  ActivityType,
+} = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+const token = process.env.DISCORD_TOKEN;
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+  client.user.setPresence({
+    activities: [{ name: "mod simulator 🤢", type: ActivityType.Streaming }],
+  });
+});
+
+client.on(Events.MessageCreate, (message) => {
+  if (message.content === "ping") {
+    message.reply("Pong!");
+  }
+  console.log(message);
+});
+
+client.login(token);
